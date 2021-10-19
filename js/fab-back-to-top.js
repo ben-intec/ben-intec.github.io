40fce975a1334b5f44dc37f4691c566d20392ee2
bendesmedt <benjamin.desmedt@student.intecbrussel.be>
2021-10-19 09:59:46 +0200

    FAB

---
+++ js/fab-back-to-top.js
@@ -0,0 +1,22 @@
+//Get the button
+const fab = document.getElementById('fab_top');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    console.log(fab);
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        fab.style.display = "block";
+    } else {
+        fab.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
